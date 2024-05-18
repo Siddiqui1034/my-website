a3f15715f50b4fd837a3eb75fca7da229d8cef97
siddiqui1034 <nausheensiddiqui11@gmail.com>
2024-05-18 20:26:58 +0530

    final-4

--- src/Home/Title/Title.jsx
+++ src/Home/Title/Title.jsx
@@ -11,7 +11,8 @@ const Title = () => {
         <div className={`titleContainerTextAnimation ${styles.titleText}`}>
           <h1>Hi, It&apos;s <span>Nausheen</span></h1>
           <h3 className={styles.textanimation}>I&apos;m a <span>Front-End Developer</span></h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum hic inventore molestias recusandae ab repudiandae pariatur porro, neque corporis fuga magnam provident, repellat asperiores debitis.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Rerum hic inventore molestias recusandae ab repudiandae pariatur porro, neque corporis fuga magnam provident, repellat asperiores debitis.</p>
          
           <div className={`my-5 text-center ${styles.btnGroup}`}>
             <Link href='#' className={styles.btn}>Resume</Link>
