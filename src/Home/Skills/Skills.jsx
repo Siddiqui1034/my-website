81cc96c88ed15b86ad8518fc14fb7060207b49be
siddiqui1034 <nausheensiddiqui11@gmail.com>
2024-05-19 10:24:00 +0530

    final-10

--- src/Home/Skills/Skills.jsx
+++ src/Home/Skills/Skills.jsx
@@ -8,28 +8,28 @@ const Skills = () => {
     <div className={`${styles.skillContainer}`}>
       <div className={styles.skillItems}>
         <div>
-          <Image className={styles.imgDiv} src="/Images/html.png" alt="" width='150' height='150' />
+          <Image className={styles.imgDiv} src="/images/html.png" alt="" width='150' height='150' />
         </div>
         <div>
-          <Image src="/Images/css.png" alt="" width='150' height='150' />
+          <Image src="/images/css.png" alt="" width='150' height='150' />
         </div>
         <div>        
-          <Image src="/Images/express.png" alt="" width='150' height='150' />
+          <Image src="/images/express.png" alt="" width='150' height='150' />
         </div>
         <div>       
-          <Image src="/Images/bootstrap.png" alt="" width='150' height='150' />
+          <Image src="/images/bootstrap.png" alt="" width='150' height='150' />
         </div>
         <div>        
-          <Image src="/Images/js.png" alt="" width='150' height='150' />
+          <Image src="/images/js.png" alt="" width='150' height='150' />
         </div>
         <div>        
-          <Image src="/Images/react.png" alt="" width='150' height='150' />
+          <Image src="/images/react.png" alt="" width='150' height='150' />
         </div>
         <div>        
-          <Image src="/Images/node.png" alt="" width='150' height='150' />
+          <Image src="/images/node.png" alt="" width='150' height='150' />
         </div>
         <div>       
-          <Image src="/Images/mongodb.png" alt="" width='150' height='150' />
+          <Image src="/images/mongodb.png" alt="" width='150' height='150' />
         </div>
       </div>
     </div>
